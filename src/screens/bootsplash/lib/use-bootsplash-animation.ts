24d1cf932cb22b35aa48225b9a2b622373839b71
Daniil Markov <mrmrkda@gmail.com>
2024-11-13 08:57:48 +1100

    refactor: bootsplash animation with config

--- src/screens/bootsplash/lib/use-bootsplash-animation.ts
+++ src/screens/bootsplash/lib/use-bootsplash-animation.ts
@@ -1,16 +1,22 @@
 import {useCallback} from 'react'
 import {
-  Easing,
+  AnimatedStyle,
   runOnJS,
+  StyleProps,
   useAnimatedStyle,
   useSharedValue,
   withSequence,
   withSpring,
   withTiming,
 } from 'react-native-reanimated'
+import {
+  OPACITY_TIMING_CONFIG,
+  SCALE_TIMING_CONFIG,
+  SPRING_CONFIG,
+} from '../config'
 
 interface BootsplashAnimationHook {
-  animatedStyles: any
+  animatedStyles: AnimatedStyle<StyleProps>
   startAnimation: (onComplete: () => void) => Promise<void>
 }
 
@@ -18,33 +24,34 @@ export const useBootsplashAnimation = (): BootsplashAnimationHook => {
   const opacity = useSharedValue(1)
   const scale = useSharedValue(1)
 
-  const animatedStyles = useAnimatedStyle(() => ({
-    opacity: opacity.value,
-    transform: [{scale: scale.value}],
-  }))
+  const animatedStyles = useAnimatedStyle(
+    () => ({
+      opacity: opacity.value,
+      transform: [{scale: scale.value}],
+    }),
+    [],
+  )
 
   const startAnimation = useCallback(async (onComplete: () => void) => {
-    scale.value = withSequence(
-      withSpring(1.02, {
-        damping: 15,
-        stiffness: 100,
+    const animations = [
+      new Promise<void>((resolve) => {
+        scale.value = withSequence(
+          withSpring(1.02, SPRING_CONFIG),
+          withTiming(0.98, SCALE_TIMING_CONFIG, () => {
+            runOnJS(resolve)()
+          }),
+        )
       }),
-      withTiming(0.98, {
-        duration: 300,
-        easing: Easing.bezier(0.25, 0.1, 0.25, 1),
-      }),
-    )
 
-    opacity.value = withTiming(
-      0,
-      {
-        duration: 800,
-        easing: Easing.bezier(0.4, 0, 0.2, 1),
-      },
-      () => {
-        runOnJS(onComplete)()
-      },
-    )
+      new Promise<void>((resolve) => {
+        opacity.value = withTiming(0, OPACITY_TIMING_CONFIG, () => {
+          runOnJS(resolve)()
+        })
+      }),
+    ]
+
+    await Promise.all(animations)
+    onComplete()
   }, [])
 
   return {
