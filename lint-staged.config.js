2b7f216bd42b1adfafd38d5ed3784b00c6c73f8e
Daniil Markov <mrmrkda@gmail.com>
2024-11-27 20:58:08 +1100

    chore: enhance lint-staged configuration to include root declaration files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,23 @@
+const {readdirSync} = require('fs')
+const {relative} = require('path')
+
+const getRootDeclarationFiles = () => {
+  const files = readdirSync('./', {withFileTypes: true})
+  return files
+    .filter((file) => !file.isDirectory() && file.name.endsWith('.d.ts'))
+    .map((file) => file.name)
+}
+
 module.exports = {
-  'src/**/*.{ts,tsx}': [() => 'tsc --noEmit', 'eslint', 'prettier --write'],
+  'src/**/*.{ts,tsx}': [
+    (files) => {
+      const cwd = process.cwd()
+      const relativePaths = files.map((file) => relative(cwd, file)).join(' ')
+      const declarationFiles = getRootDeclarationFiles()
+
+      return `npx tscw --noEmit ${relativePaths} ${declarationFiles.join(' ')}`
+    },
+    'eslint',
+    'prettier --write',
+  ],
 }
